e0fb1f13b2e3757e06f66790afb2a2a079148021
gennadiy-gaysha <gennadiy-gaysha@gmail.com>
2023-06-25 17:19:41 +0200

    Make navigation links to work using Jinja templating and routing

--- run.py
+++ run.py
@@ -18,6 +18,16 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html')
+
+
 if __name__ == '__main__':
     app.run(
         host=os.environ.get('IP', '0.0.0.0'),
