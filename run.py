0c0a8dcec2dac4625ad8ec9b9c467f93e9ef7fbf
gennadiy-gaysha <gennadiy-gaysha@gmail.com>
2023-06-25 15:56:28 +0200

    Render index.html file

--- run.py
+++ run.py
@@ -1,15 +1,25 @@
-# после инсталяции импортируем веб фреймворк Flask:
-from flask import Flask
+# Importing Flask class from flask framework
+# Rendering template function from flask
+import os
+from flask import Flask, render_template
 
-# создаем экземпляр нашего приложения через конструктор класса Flask:
-#
-# веб-сервер, на котором находится наше веб-приложение все запросы
-# от пользователей приложения будет передавать этому объекту через
-# протокол, который называется web-server gateway interface - WSGI
-# Экземпляр приложения - это объект класса Flask
+# Creating an instance of class Flask
 
+# The first argument of the Flask class, is the name of the application's module - our package.
+# Since we're just using a single module, we can use __name__ which is a built-in Python variable.
+# Flask needs this so that it knows where to look for templates and static files.
 app = Flask(__name__)
 
-# С помощью переменной __name__ фреймворк Flask сможет определить пути
-# к корневому каталогу, чтобы оттуда брать файл ресурсов
+# Telling Flask what URL should trigger the function that follows
 
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(
+        host=os.environ.get('IP', '0.0.0.0'),
+        port=int(os.environ.get('PORT', "5000")),
+        debug=True)
