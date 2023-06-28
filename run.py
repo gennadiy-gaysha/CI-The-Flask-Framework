e1644ab2221c9cb8c3d18f52c6056825da8bb2f3
gennadiy-gaysha <gennadiy-gaysha@gmail.com>
2023-06-28 18:13:02 +0200

    Add advanced link routing to member subpage

--- run.py
+++ run.py
@@ -71,6 +71,18 @@ def about():
    #  equal to the list of data it's loading from the JSON file.
     return render_template('about.html', page_title="About", company=data)
 
+@app.route("/about/<member_name>")
+def about_member(member_name):
+        member = {}
+        with open("data/company.json", 'r') as json_data:
+                data = json.load(json_data)
+                for obj in data:
+                        if obj["url"] == member_name:
+                                        member = obj
+# This first 'member' is the variable name being passed through into our html file.
+# The second 'member' is the member object we created above on line 76.
+        return render_template('member.html', member=member)
+
 
 @app.route('/contacts')
 def contacts():
